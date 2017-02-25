a866718732cdf96f7a986ca7a2eee8197d2f2e4f
birajsaha <biraj.saha@gmail.com>
2017-02-24 20:28:08 -0500

    MS SQL Server database setup script

---
+++ Trov_dbsetup_mssqlserver.sql
@@ -0,0 +1,16 @@
+﻿USE [Trov]
+GO
+
+DROP TABLE [dbo].[Items]
+CREATE TABLE [dbo].[Items] (
+    [Name]        NVARCHAR (255) NOT NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    [Price]       INT            NOT NULL
+);
+
+ALTER TABLE [dbo].[Items]
+    ADD CONSTRAINT [PK_Items] PRIMARY KEY CLUSTERED ([Name] ASC);
+
+INSERT INTO [dbo].[Items] VALUES ('Car', 'Fancy Car', 120000);
+INSERT INTO [dbo].[Items] VALUES ('Plane', 'Fancy Plane', 3000000);
+INSERT INTO [dbo].[Items] VALUES ('Ring', 'Fancy Ring', 45000);
